5aa75d02e7d73a844932a09eb02612bfe31eadaf
Apoorv Garg <u.garg.10@gmail.com>
2020-11-17 21:10:53 +0530

    DjangoRecaptcha

---
+++ DjangoRecaptcha/Recaptcha/urls.py
@@ -0,0 +1,10 @@
+  
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+	path('',views.home,name="home"),
+	path('recaptcha/', views.comments, name="recaptcha"),
+]
---
+++ DjangoRecaptcha/Recaptcha/views.py
@@ -0,0 +1,42 @@
+import json
+import urllib
+
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+
+from .models import Comment
+from .forms import CommentForm
+
+def home(request):
+    return render(request, 'index.html')
+
+def comments(request):
+    comments_list = Comment.objects.order_by('-created_at')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req =  urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+
+            if result['success']:
+                form.save()
+                messages.success(request, 'New comment added with success!')
+            else:
+                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+
+            return redirect('')
+    else:
+        form = CommentForm()
+
+    return render(request, 'RecaptchaForm.html', {'comments': comments_list, 'form': form})
